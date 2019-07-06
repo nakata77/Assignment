3f359b731b9629cf126ed765c53f410c06cd41fe
nakata77 <sagittarian@abv.bg>
2019-07-06 15:16:06 +0300

    Code improvement

--- nbaplayers/Program.cs
+++ nbaplayers/Program.cs
@@ -1,93 +1,102 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace nbaplayers
 {
     class Program
     {
-        private static string _enetredData;
+        private enum Choise
+        {
+            OpenFile,
+            ChoosePath,
+            Years,
+            Rating
+        }
+
+        private static string _enteredData;
 
         private static int _maxYears, _minRating, _number;
-
-        private static bool _cond;
-
-        private static void ChangeCondition(string arg, string enteredData)
-        {
-            switch (arg)
-            {
-                case "openFile":
-                    _cond = File.Exists(_enetredData) && Path.GetExtension(_enetredData) == ".json";
-                    break;
-                case "choosePath":
-                    _cond = Directory.Exists(_enetredData);
-                    break;
-                default:
-                    _cond = int.TryParse(enteredData, out _number);
-
-                    if (arg == "years")
-                        _maxYears = _number;
-                    else
-                        _minRating = _number;
-
-                    break;
-            }
-        }
-
-        private static void CheckData(ref string enteredData, string condition, string retryMessage, 
-            string conditionMessage)
-        {
-            Console.WriteLine(conditionMessage);
-            while (true)
-            {
-                enteredData = Console.ReadLine();
-                ChangeCondition(condition, enteredData);
-                if (_cond)
-                    break;
-
-                Console.WriteLine(retryMessage);
-            }
-        }
 
         static void Main(string[] args)
         {
             List<Player> players;
 
-            CheckData(ref _enetredData, "openFile", "Wrong path or file! Try again: ",
+            CheckData(ref _enteredData, Choise.OpenFile, "Wrong path or file! Try again. ",
                 "Input path to JSON file: ");
 
-            using (var r = new StreamReader(_enetredData))
+            using (var file = new StreamReader(_enteredData))
             {
-                var json = r.ReadToEnd();
+                var json = file.ReadToEnd();
                 players = JsonConvert.DeserializeObject<List<Player>>(json);
             }
 
-            CheckData(ref _enetredData, "years","The entered information is not a year. Try again: ",
+            CheckData(ref _enteredData, Choise.Years,"The entered information is not a year. Try again. ",
                 "Input maximum number of years the player has played in the league to qualify: ");
 
-            CheckData(ref _enetredData, "rating","The entered information is not a number. Try again: ",
+            CheckData(ref _enteredData, Choise.Rating,"The entered information is not a number. Try again. ",
                 "Input minimum rating the player should have to qualify: ");
 
-            CheckData(ref _enetredData, "choosePath", "Directory does not exist! Try again: ",
+            CheckData(ref _enteredData, Choise.ChoosePath, "Directory does not exist! Try again. ",
                 "Input path for the CSV file: ");
 
-            var csvFile = _enetredData + "\\generatedCsv.txt";
+            var csvFile = _enteredData + "\\generatedCsv.txt";
 
-            if (File.Exists(csvFile))
-                File.Delete(csvFile);
-
-            using (var file = new StreamWriter(csvFile, true))
+            using (var file = new StreamWriter(csvFile))
             {
                 file.WriteLine("Name, Rating");
 
-                foreach (var player in players)
+                IOrderedEnumerable<Player> sortedPlayers = players.OrderByDescending(p => p.Rating);
+
+                foreach (var player in sortedPlayers)
                 {
                     if (DateTime.Now.Year - player.PlayingSince <= _maxYears && player.Rating >= _minRating)
                         file.WriteLine(player.Name + ", " + player.Rating);
                 }
 
                 Console.WriteLine("File generated! Check it at: " + csvFile);
+            }
+
+            Process.Start(csvFile);
+        }
+
+        private static bool ChangeCondition(Choise condition, string enteredData)
+        {
+            bool cond;
+            switch (condition)
+            {
+                case Choise.OpenFile:
+                    cond = File.Exists(_enteredData) && Path.GetExtension(_enteredData) == ".json";
+                    return cond;
+                case Choise.ChoosePath:
+                    cond = Directory.Exists(_enteredData);
+                    return cond;
+                default:
+                    cond = int.TryParse(enteredData, out _number);
+
+                    if (condition == Choise.Years)
+                        _maxYears = _number;
+                    else
+                        _minRating = _number;
+
+                    return cond;
+            }
+        }
+
+        private static void CheckData(ref string enteredData, Choise condition, string retryMessage,
+            string conditionMessage)
+        {
+            Console.WriteLine(conditionMessage);
+            while (true)
+            {
+                enteredData = Console.ReadLine();
+                if (ChangeCondition(condition, enteredData))
+                    break;
+
+                Console.WriteLine(retryMessage);
             }
         }
     }
